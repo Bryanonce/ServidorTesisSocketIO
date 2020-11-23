b956b7b3a40add787d18a2270a99b975842f6e8c
Bryan <bradcede@espol.edu.ec>
2020-11-23 16:54:30 -0500

    tokens Auth

--- dist/global/config.js
+++ dist/global/config.js
@@ -4,4 +4,4 @@ exports.CLIENTE = exports.SEMILLA = exports.URL_DATABASE = exports.SERVER_PORT =
 exports.SERVER_PORT = Number(process.env.PORT) || 5000;
 exports.URL_DATABASE = process.env.MONGO_URI || 'mongodb://localhost:27017/tesis';
 exports.SEMILLA = process.env.SEED || 'semilla-de-prueba';
-exports.CLIENTE = process.env.CLIENV || '1003353693226-l1b3muqg00hjnr0v50lc28sgjvr7qv0b.apps.googleusercontent.com';
+exports.CLIENTE = '1003353693226-5p1okcfq6r6ivp1oe99o8o7omfcvu41m.apps.googleusercontent.com';
