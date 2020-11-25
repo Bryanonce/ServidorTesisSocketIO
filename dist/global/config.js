e16f3385eaa027a4b1d3ab3795adcc327ef8b6b2
Bryan <bradcede@espol.edu.ec>
2020-11-25 16:05:11 -0500

    Aplicacion

--- dist/global/config.js
+++ dist/global/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CADUCIDAD = exports.CLIENTE = exports.SEMILLA = exports.URL_DATABASE = exports.SERVER_PORT = void 0;
 exports.SERVER_PORT = Number(process.env.PORT) || 5000;
-exports.URL_DATABASE = String(process.env.MONGO_URI) || 'mongodb://localhost:27017/tesis';
+exports.URL_DATABASE = 'mongodb://localhost:27017/tesis';
 exports.SEMILLA = String(process.env.SEED) || 'semilla-de-prueba';
 exports.CLIENTE = String(process.env.CLIENV);
 exports.CADUCIDAD = Number(process.env.CAD_TOKEN) || 60 * 60 * 24 * 30;
