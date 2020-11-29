a1900458e5a17645cb0ede05a2b75f42d3f6a6d1
Bryan <bradcede@espol.edu.ec>
2020-11-29 18:21:07 -0500

    Seguridad para administracion

--- global/config.ts
+++ global/config.ts
@@ -1,6 +1,6 @@
 export const SERVER_PORT:number = Number(process.env.PORT) || 5000;
 
-export const URL_DATABASE:string = /*String(process.env.MONGO_URI) || */'mongodb://localhost:27017/tesis'
+export const URL_DATABASE:string = String(process.env.MONGO_URI) || 'mongodb://localhost:27017/tesis'
 
 export const SEMILLA:string = String(process.env.SEED) || 'semilla-de-prueba';
 
